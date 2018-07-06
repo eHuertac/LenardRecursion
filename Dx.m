28b933d310f67b143b8b6be6a2fc8d92ebf97b45
Eduardo Huerta <eduardojhuerta@gmail.com>
2018-07-06 12:57:39 -0300

    Update Dx.m

--- Dx.m
+++ Dx.m
@@ -1,9 +1,9 @@
 function [ N, EXPR ] = Dx(m, n, expr )
-%DerivadaTotal Definición del operador "Derivada total"y su "inversa"
+%DerivadaTotal DefiniciÃ³n del operador "Derivada total"y su "inversa"
 %   Dx := dx + u1 du0 + u2 du1 + u3 du2 + ...
     N = n;
     EXPR = expr;
-    while(m > 0)
+    while(m > 0) %% Derivando totalmente hasta un orden m
         n = N;
         expr = EXPR;  %%Truco para no perder el valor de EXPR y N(si m!=1)
         EXPR = diff(expr,'x') + diff(expr,'u0');     %% Dx := dx + u1 du0 + <- derivando parcial respecto a x
@@ -12,15 +12,15 @@ function [ N, EXPR ] = Dx(m, n, expr )
             EXPR = EXPR + sym(strcat('u',int2str(n+1)))*diff(expr,strcat('u',int2str(n)));
             n = n - 1; 
         end
-        m = m - 1; % m es cuántas veces aplicar Dx
+        m = m - 1; % m es cuÃ¡ntas veces aplicar Dx
     end
-    while(m < 0) %%Aquí está la importancia matemática de que el sistema sea integrable, sino no funciona el algoritmo. 
+    while(m < 0) %%AquÃ­ estÃ¡ la importancia matemÃ¡tica de que el sistema sea integrable, sino no funciona el algoritmo. 
         n = N; 
         expr = EXPR;
         N = n-1;
         
         while(n ~= 0)
-            EXPR = int(expr)
+            EXPR = int(expr) %%EXPRESION INCOMPLETA!
     end
         
 end
